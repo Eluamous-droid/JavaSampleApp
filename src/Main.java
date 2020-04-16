8fa6c8eed9eb7deca29b63f54d814d42b8f85d62
Mikkel Hansen <themik1337@gmail.com>
2020-04-16 23:05:10 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("Sucks to suck");
+            try {
+                Thread.sleep(60000);
+            }catch (Exception e){
+
+            }
+        }
+    }
+}
