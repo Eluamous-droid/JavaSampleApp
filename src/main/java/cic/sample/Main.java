ebb78b08390c2744f6dc60f6655df4c42acce3f5
Mikkel Hansen <themik1337@gmail.com>
2020-09-04 21:00:26 +0200

    remove loop

--- src/main/java/cic/sample/Main.java
+++ src/main/java/cic/sample/Main.java
@@ -2,13 +2,6 @@ package cic.sample;
 public class Main {
 
     public static void main(String[] args) {
-        while(true) {
             System.out.println("Sucks to suck");
-            try {
-                Thread.sleep(600);
-            }catch (Exception e){
-
-            }
-        }
     }
 }
